38df51eb79ee0571cd3886657ace3f243b32aeb6
Quartal <docky563@gmail.com>
2023-04-21 00:57:45 +0700

    Added branch and mirror function

--- MiaoProject/Program.cs
+++ MiaoProject/Program.cs
@@ -8,13 +8,14 @@ class Programm
         Console.WriteLine("Ваше отражение");
         Console.WriteLine("Дата и время");
         Console.WriteLine("Пожелание на день");
+        Console.WriteLine();
         switch (Console.ReadKey().Key)
         {
             case ConsoleKey.D1:
                 
                 break;
             case ConsoleKey.D2:
-                
+                new Mirror().ShowMyFace();
                 break;
             case ConsoleKey.D3:
                 
